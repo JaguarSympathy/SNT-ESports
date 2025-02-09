5fe47cbeee3dedeba75ef5d0bda7bbe5306cff31
JaguarSympathy <director.apollosystems@gmail.com>
2025-02-09 10:59:00 +0000

    Added leveling

--- bot.py
+++ bot.py
@@ -19,6 +19,7 @@ load_dotenv()
 TOKEN = os.getenv("DISCORD_CLIENT_TOKEN")
 WELCOME_CHANNEL = 000000000000000000
 IMG_WELCOME = "https://media.discordapp.net/attachments/1336054298400002108/1337101833629466625/standard_8.gif"
+LEVELING_REQUIREMENT = 100
 
 # -- Initialisation -- #
 client = discord.Client(intents=discord.Intents.all())
@@ -43,12 +44,33 @@ async def on_member_join(member: discord.Member):
     embed.set_image(IMG_WELCOME)
     await channel.send(f"Welcome {member.mention}!", embed=embed)
 
+@client.event
+async def on_message(message: discord.Message):
+    if message.author.bot == False:
+        with open("leveling.json","r") as f:
+            levelingData = json.load(f)
+
+        if message.author.id not in levelingData:
+            levelingData[message.author.id] = {"xp":0,"level":0}
+        else:
+            levelingData[message.author.id]["xp"] += 1
+
+        level = levelingData[message.author.id]["xp"] // LEVELING_REQUIREMENT
+        if level > levelingData[message.author.id]["level"]:
+            levelingData[message.author.id]["level"] = level
+
+            await message.channel.send(f"Congratulations {message.author.mention}! You have reached level {level}!")
+
+        with open("leveling.json","w") as f:
+            json.dump(levelingData,f)
+
+
 # -- Commands -- #
 @tree.command(name="settings",description="Update bot settings")
 @app_commands.describe(setting="Setting to update",value="Value to set")
-@app_commands.choices(setting=[app_commands.Choice(name="Welcome Channel",value="Welcome Channel"),app_commands.Choice(name="Welcome Image",value="Welcome Image")])
+@app_commands.choices(setting=[app_commands.Choice(name="Welcome Channel",value="Welcome Channel"),app_commands.Choice(name="Welcome Image",value="Welcome Image"),app_commands.Choice(name="Leveling Requirement",value="Leveling Requirement")])
 async def settings(interaction: discord.Interaction, setting: str, value: str):
-    global WELCOME_CHANNEL,IMG_WELCOME
+    global WELCOME_CHANNEL,IMG_WELCOME,LEVELING_REQUIREMENT
     if setting == "Welcome Channel":
         WELCOME_CHANNEL = value
         with open("settings.json","w") as f:
@@ -59,9 +81,15 @@ async def settings(interaction: discord.Interaction, setting: str, value: str):
         IMG_WELCOME = value
         with open("settings.json","w") as f:
             json.dump({"welcome_channel":WELCOME_CHANNEL,"img_welcome":IMG_WELCOME},f)
-
+        
         await interaction.response.send_message("Updated welcome image.",ephemeral=True)
+    elif setting == "Leveling Requirement":
+        LEVELING_REQUIREMENT = value
+        with open("settings.json","w") as f:
+            json.dump({"welcome_channel":WELCOME_CHANNEL,"img_welcome":IMG_WELCOME,"leveling_requirement":LEVELING_REQUIREMENT},f)
+
+        await interaction.response.send_message("Updated leveling requirement.",ephemeral=True)
     else:
-        await interaction.response.send_message("Invalid setting",ephemeral=True)
+        await interaction.response.send_message("Invalid setting.",ephemeral=True)
 
 client.run(TOKEN)
